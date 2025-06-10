a15077843519dbf5a94bbe583e43542db8a0cd2c
Dwane Dunn <dwane.dunn@gmail.com>
2025-06-10 17:34:45 +0000

    setup components

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 //Bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 // index.css'
-import '../styles/index.css'
+import '../styles/index.css';
 
 // components
 import Home from './components/Home';
-import {App} from ./components/App
+import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Home/> */}
-    <App/>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
