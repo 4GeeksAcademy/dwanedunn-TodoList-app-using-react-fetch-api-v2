40abb4c38ec12d87ffca3e6d093fd006540bb3ee
Dwane Dunn <dwane.dunn@gmail.com>
2025-06-10 16:57:57 +0000

    build todoItem, todoList

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,13 +1,34 @@
 import { useState, useEffect } from 'react';
 
-export function App() {
-  const [todos, SetTodos] = useState([]);
+function App() {
+  const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
 
-  export addTodo = () => {
-    if (newTodo.trim()) {
-      SetTodos([...todos, { text: newTodo, completed: false }]);
-      setNewTodo('');
-    }
+  const toggleTodo = (index) => {
+    const updatedTodos = todos.map((todo, i) =>
+      i === index ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
   };
+
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={addTodo}>Add Todo</button>
+
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </div>
+  );
 }
+
+export default App;
--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -1,1 +1,19 @@
-import React, {useState, useEffect} from "react";
+import React from 'react';
+import TodoItem from './TodoItem';
+
+function TodoList({ todos, toggleTodo, deleteTodo }) {
+  return (
+    <ul>
+      {todos.map((todo, index) => (
+        <TodoItem
+          key={index}
+          todo={todo}
+          toggleTodo={() => toggleTodo(index)}
+          deleteTodo={() => deleteTodo(index)}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
